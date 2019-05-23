b972439678228865af587d8f4df68597fb907d87
wangz <wangz@jfinfo.com>
2019-05-23 13:57:28 +0800

    flag demo

---
+++ flag.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+var name string
+func init() {
+	//flag 引用命令行传入的参数 例: flag -n king 或 flag -n=king
+	flag.StringVar(&name, "n", "default", "")
+}
+
+func main() {
+	//flag用来解析命令行参数 flag.Parse() 执行这个方法可以将参数赋值给引用好的变量
+	flag.Parse()
+	fmt.Fprint(os.Stderr, "hello world\n")
+	fmt.Println(name)
+}
